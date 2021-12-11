d8ec445fa97d7fb69ce0f9d120d1c1c5e9438330
Cemre <91261937+ceemrekarsu@users.noreply.github.com>
2021-12-12 00:53:37 +0300

    document object model exercise

---
+++ 11.12.2021/document-object-model.js
@@ -0,0 +1,14 @@
+let title = document.getElementById('title')
+title.innerHTML =  "shit happens"
+console.log(title.innerHTML)
+title.style.textAlign =  "center"
+
+let link = document.querySelector("ul#list>li>a")   
+link.innerHTML +=  " <br> hey"
+link.style.color =  "orange"
+link.classList.add('btn')
+link.style.textDecoration =  "none"
+list.style.textAlign =  "center"
+list.style.listStyleType =  "none"
+
+
